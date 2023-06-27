324b6f29522071c5dab276d6f5d7a0452348f39d
qixuan <18813314681@163.com>
2023-06-27 13:58:38 +0800

    master commit

--- src/main/java/com/qixuan/GitTest.java
+++ src/main/java/com/qixuan/GitTest.java
@@ -2,9 +2,8 @@ package com.qixuan;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!333");
-        System.out.println("hello git!hot-fix分支");
-        System.out.println("hello git!hot-fix分支");
-        System.out.println("hello git!hot-fix分支");
+        System.out.println("IDEA集成Git");
+        System.out.println("hello git!master分支");
+        System.out.println("hello git!master分支");
     }
 }
