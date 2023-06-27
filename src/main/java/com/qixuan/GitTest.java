06b0659be02fbbb96e5c13fd31bbac4ca18448ca
qixuan <18813314681@163.com>
2023-06-27 11:32:34 +0800

    second commit

--- src/main/java/com/qixuan/GitTest.java
+++ src/main/java/com/qixuan/GitTest.java
@@ -3,5 +3,7 @@ package com.qixuan;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 }
