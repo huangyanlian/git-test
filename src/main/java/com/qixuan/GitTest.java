54f4b1c1c782d71f0043b47f7e9efb16b952ae67
qixuan <18813314681@163.com>
2023-06-27 13:46:15 +0800

    hot-fix first commit

--- src/main/java/com/qixuan/GitTest.java
+++ src/main/java/com/qixuan/GitTest.java
@@ -3,7 +3,8 @@ package com.qixuan;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!333");
-        System.out.println("hello git!333");
-        System.out.println("hello git!333");
+        System.out.println("hello git!hot-fix分支");
+        System.out.println("hello git!hot-fix分支");
+        System.out.println("hello git!hot-fix分支");
     }
 }
